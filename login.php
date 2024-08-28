c695446d207e7ddf77948bd5fc57a38a33eed659
PabloSchlender <pablitoschlender01@gmail.com>
2024-08-28 13:08:18 -0300

    aspecto visual del login

---
+++ login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #f8f9fa;
+        }
+        .login-container {
+            width: 100%;
+            max-width: 400px;
+            padding: 15px;
+            border: 1px solid #dee2e6;
+            border-radius: 5px;
+            background-color: #ffffff;
+        }
+    </style>
+</head>
+<body>
+<div class="card" style="width: 18rem;">
+<div class="login-container">
+        <h2 class="text-center">Login</h2>
+            <div class="form-group">
+                <label for="username">Usuario</label>
+                <input type="text" class="form-control" id="username" name="username" required>
+            </div>
+            <br/>
+            <div class="form-group">
+                <label for="password">Contraseña</label>
+                <input type="password" class="form-control" id="password" name="password" required>
+            </div>
+            <br/>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+            <button type="button" class="btn btn-dark">Login</button>
+    </div>
+    </div>
+</div>
+</body>
+</html>
